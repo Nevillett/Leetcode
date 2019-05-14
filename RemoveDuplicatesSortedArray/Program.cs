8d638889b23ece341aacd31387e311af4ac4b5ff
Kyler Tian <ktian@vrs.state.va.us>
2019-05-14 17:18:53 -0400

    Hashset solution

---
+++ RemoveDuplicatesSortedArray/Program.cs
@@ -0,0 +1,22 @@
+﻿// 26
+
+using System;
+using System.Collections.Generic;
+
+namespace RemoveDuplicatesSortedArray
+{
+    class Program
+    {   
+        public static int RemoveDuplicatesSortedArray(int[] nums)
+        {
+            var hash = new HashSet<int>(nums);
+            return hash.Count;
+        }
+        static void Main(string[] args)
+        {
+            int[] nums = new int[]{1,1,2,2,3,5,5,5};
+            var result = RemoveDuplicatesSortedArray(nums);
+            Console.WriteLine(result);
+        }
+    }
+}
