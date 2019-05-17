e0bb2db5f0f2677ade9df9004649a7dd84996e5f
Kyler Tian <nevillett8@gmail.com>
2019-05-17 14:24:48 -0400

    solution 2; two pointer and better understanding of ++pre and cur++

--- RemoveDuplicatesSortedArray/Program.cs
+++ RemoveDuplicatesSortedArray/Program.cs
@@ -12,11 +12,27 @@ namespace RemoveDuplicatesSortedArray
             var hash = new HashSet<int>(nums);
             return hash.Count;
         }
+
+        public static int RemoveDuplicatesSortedArray2(int[] nums)
+        {
+            int pre = 0, cur = 0, l = nums.Length;
+            while(cur < l)
+            {
+                if (nums[pre] == nums[cur]) ++cur;
+                else nums[++pre] = nums[cur++];
+                // better understanding ++pre and cur++
+                Console.WriteLine("pre:{0}; cur:{1}", pre, cur);
+                Console.WriteLine(string.Join(",", nums));                
+            }
+            return pre + 1;
+        }
         static void Main(string[] args)
         {
             int[] nums = new int[]{1,1,2,2,3,5,5,5};
             var result = RemoveDuplicatesSortedArray(nums);
+            var result2 = RemoveDuplicatesSortedArray2(nums);
             Console.WriteLine(result);
+            Console.WriteLine(result2);
         }
     }
 }
