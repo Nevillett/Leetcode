8d3cd22bccdef32892a939ed1364897a9353a5c6
Kyler Tian <nevillett8@gmail.com>
2019-05-20 10:00:39 -0400

    solution 3

--- RemoveDuplicatesSortedArray/Program.cs
+++ RemoveDuplicatesSortedArray/Program.cs
@@ -15,6 +15,7 @@ namespace RemoveDuplicatesSortedArray
 
         public static int RemoveDuplicatesSortedArray2(int[] nums)
         {
+            if (nums == null || nums.Length == 0) return 0;
             int pre = 0, cur = 0, l = nums.Length;
             while(cur < l)
             {
@@ -26,13 +27,28 @@ namespace RemoveDuplicatesSortedArray
             }
             return pre + 1;
         }
+
+        public static int RemoveDuplicatesSortedArray3(int[] nums)
+        {
+            if (nums == null || nums.Length == 0) return 0;
+            int j = 0; int n = nums.Length;
+            for (int i = 0; i < n; i++)
+            {
+                Console.Write(i);
+                if (nums[i] != nums[j]) 
+                nums[++j] = nums[i];                
+            }
+            return j + 1;
+        }
         static void Main(string[] args)
         {
             int[] nums = new int[]{1,1,2,2,3,5,5,5};
             var result = RemoveDuplicatesSortedArray(nums);
             var result2 = RemoveDuplicatesSortedArray2(nums);
+            var result3 = RemoveDuplicatesSortedArray3(nums);
             Console.WriteLine(result);
             Console.WriteLine(result2);
+            Console.WriteLine(result3);
         }
     }
 }
