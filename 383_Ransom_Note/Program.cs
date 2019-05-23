2bdc2ca31eb362f17c910fb5c3ebab21a10f9292
Kyler Tian <nevillett8@gmail.com>
2019-05-23 14:37:04 -0400

    solution 2 :clarity way

--- 383_Ransom_Note/Program.cs
+++ 383_Ransom_Note/Program.cs
@@ -29,12 +29,32 @@ namespace _383_Ransom_Note
             }
             return true;
         }
+        //solution 2: easy and clearly
+        public static bool canConstruct2(string ransomNote, string magazine)
+        {
+            Dictionary<char, int> dic = new Dictionary<char, int>();
+            foreach( char c in magazine)
+            {
+                if (dic.ContainsKey(c)) 
+                ++dic[c];
+                else
+                dic.Add(c,1);                
+            }
+            foreach ( char c in ransomNote)
+            {
+                if (--dic[c] < 0) 
+                return false;
+            }
+            return true;
+        }
         static void Main(string[] args)
         {
             string ransomNote = "abb";
             var magazine = "aabb";
             var result = canConstruct(ransomNote, magazine);
+            var result2 = canConstruct2(ransomNote, magazine);
             Console.WriteLine(result);
+            Console.WriteLine(result2);
         }
     }
 }
