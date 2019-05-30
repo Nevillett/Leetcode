8f03a72616a1e14446ff7b7c2a199cd988dd223d
Kyler Tian <nevillett8@gmail.com>
2019-05-30 15:17:06 -0400

    two solution: 1. array.reverse & loop reverse

---
+++ 344_Reverse_String/Program.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Text;
+
+namespace _344_Reverse_String
+{
+    class Program
+    {
+        public static string reverseString(char[] s)    
+        {
+            StringBuilder sb = new StringBuilder();
+            foreach( char c in s)
+            {
+                sb.Insert(0, c);
+            }
+            return sb.ToString();
+        }
+       
+       public static string reverseString2(char[] s)
+       {
+           Array.Reverse(s);
+           return new string(s);           
+       }
+
+       public static string reverseString3(char[] s)
+       {
+           int left = 0, right = s.Length - 1;
+           while (left < right)
+           {
+               char t = s[left];
+               s[left++] = s[right];
+               s[right--] = t;
+           }
+           return new string (s);
+       }
+        static void Main(string[] args)
+        {
+            char[] s = new char[] {'h', 'e', 'l', 'l', 'o'};
+            // just test :......
+            var x= s.ToString();
+            Console.WriteLine(x);
+            Console.WriteLine(new string(s)[1]);
+            Console.WriteLine("*************");
+
+            var result = reverseString(s);    
+            var result2 = reverseString2(s);
+            var result3 = reverseString3(s);            
+
+            Console.WriteLine(result);
+            Console.WriteLine(result2);
+            Console.WriteLine(result3);
+        }
+    }
+}
