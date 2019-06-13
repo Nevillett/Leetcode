d51ab3c8d2b76cbb02b248c199dfacdb25fcf439
Kyler Tian <nevillett8@gmail.com>
2019-06-13 18:06:53 -0400

    151 two solution

---
+++ 151_Reverse_Words_inString/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Text;
+
+namespace _151_Reverse_Words_inString
+{
+    class Program
+    {
+        public static string ReverseWordsInString(string s)
+        {
+            string[] words = s.Trim().Split(' ');
+            Array.Reverse(words);
+            return String.Join(" ", words);
+        }
+        public static string ReverseWordsInString2(string s)
+        {
+            string[] words = s.Trim().Split(' ');
+            StringBuilder sb = new StringBuilder();
+            for (int i = words.Length - 1; i >= 0; i--)
+            {
+                //StringBuilder sb = new StringBuilder();
+                sb.Append(words[i]);                
+                sb.Append(" ");
+            }
+            return sb.ToString();        
+        }
+
+        static void Main(string[] args)
+        {
+            string s = "the sky is blue ";
+            var result = ReverseWordsInString(s);
+            Console.WriteLine(result);
+            var result2 = ReverseWordsInString2(s);
+            Console.WriteLine(result2);
+        }
+    }
+}
