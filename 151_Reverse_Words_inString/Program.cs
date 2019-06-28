ef32fb30cf3a03458a718d0b8c4903189293385f
Kyler Tian <nevillett8@gmail.com>
2019-06-28 15:56:42 -0400

    Trim and replace; Regex.Split() regular expression

--- 151_Reverse_Words_inString/Program.cs
+++ 151_Reverse_Words_inString/Program.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Text;
+using System.Text.RegularExpressions;
 
 namespace _151_Reverse_Words_inString
 {
@@ -7,12 +8,14 @@ namespace _151_Reverse_Words_inString
     {
         public static string ReverseWordsInString(string s)
         {
-            string[] words = s.Trim().Split(' ');
+            // using regular expression to split extra whitespace in the middle of the string
+            string[] words =  Regex.Split( s.Trim() , @"\s+" );   // or @" +"
             Array.Reverse(words);
             return String.Join(" ", words);
         }
         public static string ReverseWordsInString2(string s)
         {
+            // not using regex to split, output wrong when there are whitespaces in the middle of the string 
             string[] words = s.Trim().Split(' ');
             StringBuilder sb = new StringBuilder();
             for (int i = words.Length - 1; i >= 0; i--)
@@ -23,14 +26,25 @@ namespace _151_Reverse_Words_inString
             }
             return sb.ToString();        
         }
+        public static void TrimandReplace()
+        {
+            var tmp = "   hello    world   ";
+            var res1 = tmp.Trim();
+            var res2 = tmp.Replace(" ", String.Empty);
+            System.Console.WriteLine(res1);
+            System.Console.WriteLine(res2);
+        }
 
         static void Main(string[] args)
         {
-            string s = "the sky is blue ";
+            string s = " hello      world!  ";
             var result = ReverseWordsInString(s);
             Console.WriteLine(result);
             var result2 = ReverseWordsInString2(s);
             Console.WriteLine(result2);
+            Console.WriteLine("*****************");
+            TrimandReplace();
+
         }
     }
 }
