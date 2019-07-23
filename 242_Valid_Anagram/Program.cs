00d52561ec2f4bcfdc04693414691aeefc112446
Kyler Tian <nevillett8@gmail.com>
2019-07-23 10:59:59 -0400

    242_solution1 array sort

---
+++ 242_Valid_Anagram/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace _242_Valid_Anagram
+{
+    class Program
+    {
+        public static bool IsAnagram (string s, string t) {
+            if (s.Length != t.Length)
+            return false;
+
+            char[] source = s.ToCharArray(), target = t.ToCharArray();
+
+            Array.Sort(source);
+            Array.Sort(target);
+
+            for (int i = 0; i < s.Length; i++) {
+                if (source[i] != target[i])
+                    return false;
+            }
+            return true;
+        }
+        static void Main(string[] args)
+        {
+            string s = "rat";
+            string t = "car";
+            var result = IsAnagram(s, t);
+
+            Console.WriteLine(result);
+        }
+    }
+}
