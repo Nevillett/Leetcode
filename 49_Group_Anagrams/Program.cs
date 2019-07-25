6d3a22ae2676a115f758b953a0e1ca70bb783d6c
Kyler Tian <nevillett8@gmail.com>
2019-07-25 17:04:33 -0400

    solution2 using one loop

--- 49_Group_Anagrams/Program.cs
+++ 49_Group_Anagrams/Program.cs
@@ -23,7 +23,7 @@ namespace _49_Group_Anagrams
                 // key point here: no matter the dictionary has key or not, add the string in the list
                 dc[listStrs[i]].Add(strs[i]);                
             }
-            // get all the value from dictionary
+            // get all the value from dictionary;  List<IList> should pay attention; 
             return new List<IList<string>>(dc.Values);
             // var result = new List<IList<string>>();
             // foreach (var item in dc) {
@@ -31,10 +31,26 @@ namespace _49_Group_Anagrams
             // }
             // return result;
         }
+        // solution 2: using only one loop, foreach
+        public static IList<IList<string>> GroupAnagrams2(string[] strs) {
+            Dictionary<string, List<string>> dc = new Dictionary<string, List<string>>();
+
+            foreach (var item in strs) {
+                var charArray = item.ToCharArray();
+                Array.Sort(charArray);
+                string sortStr = new string(charArray);
+
+                if (!dc.ContainsKey(sortStr)) {
+                    dc.Add(sortStr, new List<string>());
+                }
+                dc[sortStr].Add(item);
+            }
+            return new List<IList<string>>(dc.Values);
+        }
         static void Main(string[] args)
         {
             string[] strs = new string[]{"eat", "tea", "tan", "ate", "nat", "bat"};
-            var result = GroupAnagrams(strs);
+            var result = GroupAnagrams2(strs);
             foreach (var item in result) {
                 Console.WriteLine(string.Join(", ", item));
             }            
