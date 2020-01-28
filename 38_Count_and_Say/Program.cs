53ad779266d260cdcce76e6fc34465c42aec8ab0
Nevillett <nevillett8@gmail.com>
2020-01-28 10:23:17 -0500

    test
    test

---
+++ 38_Count_and_Say/Program.cs
@@ -0,0 +1,21 @@
+﻿// 1.     1
+// 2.     11
+// 3.     21
+// 4.     1211
+// 5.     111221
+// 1 is read off as "one 1" or 11.
+// 11 is read off as "two 1s" or 21.
+// 21 is read off as "one 2, then one 1" or 1211.
+
+using System;
+
+namespace _38_Count_and_Say
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
