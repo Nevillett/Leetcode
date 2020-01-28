f9ca0d676b5aa36743c6532a21a680b8202379b3
Nevillett <nevillett8@gmail.com>
2020-01-28 10:58:16 -0500

    Add 274, 275 H-Index

---
+++ 274_H-Index/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace _274_H_Index
+{
+    class Program
+    {   
+        public static int HIndex(int[] citations) {
+            Array.Sort(citations);
+            Array.Reverse(citations);
+            Console.WriteLine(string.Join("," , citations));
+            for (int i = 0; i < citations.Length; i++) {
+                if (i >= citations[i]) 
+                return i;
+            }
+            return citations.Length;
+        }
+        static void Main(string[] args)
+        {
+            int[] citations = new int[]{3,0,6,1,5};
+            var result = HIndex(citations);
+            Console.WriteLine(result);
+        }
+    }
+}
